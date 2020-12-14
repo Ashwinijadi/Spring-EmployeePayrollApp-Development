4820157f76dff5f906eb5c01678df46c4af6c01f
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-12-14 09:05:59 -0800

    added : Setting up database as environment variable is added

--- main/java/com/capgemini/employeepayrollapp/EmployeePayrollAppApplication.java
+++ main/java/com/capgemini/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -10,9 +10,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class EmployeePayrollAppApplication {
 
-	public static void main(String[] args) {
-		ApplicationContext context=SpringApplication.run(EmployeePayrollAppApplication.class, args);
-		
-		log.info("Employee Payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));
-	}
+		public static void main(String[] args) {
+			ApplicationContext context=SpringApplication.run(EmployeePayrollAppApplication.class, args);
+			log.info("Employee Payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));
+			log.info("Employee Payroll DB User is {}",context.getEnvironment().getProperty("spring.datasource.username"));
+		}
 }
