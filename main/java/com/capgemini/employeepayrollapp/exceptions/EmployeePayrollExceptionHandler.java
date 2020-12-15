54e94e6ccc4b1b30ab6b34486b5f2c3c1abf3297
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-12-15 09:29:18 -0800

    added :Exception handler for Incorrect date format is added

--- main/java/com/capgemini/employeepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
+++ main/java/com/capgemini/employeepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
@@ -8,11 +8,25 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 
 import com.capgemini.employeepayrollapp.dto.ResponseDTO;
 
+import lombok.extern.slf4j.Slf4j;
+
 @ControllerAdvice
+@Slf4j
 public class EmployeePayrollExceptionHandler {
+	private static final String message = "Exception while processing REST Request";
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<ResponseDTO> handleHttpMessageNotReadableException(
+			HttpMessageNotReadableException exception) {
+		log.error("Invalid Date Format", exception);
+		ResponseDTO responseDTO = new ResponseDTO(message, "Should have date in the format dd MM yyyy");
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+	}
+
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
 			MethodArgumentNotValidException exception) {
